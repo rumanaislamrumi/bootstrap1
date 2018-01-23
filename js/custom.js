97646ca621ab5374d751ada1238d089801f3a925
rumanaislamrumi <rumanaislam.tkg@gmail.com>
2018-01-23 10:10:53 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+
+ $(document).ready(function(){
+    $(".navbar").sticky({topSpacing:0});
+  });
+  
+ $('body').scrollspy({ target: '#bs-example-navbar-collapse-1' })
+ 
+$(document).ready(function(){
+  new WOW().init()
+	// sticky nav jQuery activation
+    $(".navbar").sticky({topSpacing:0});
+    
+    // smooth scroll activation
+
+    // Select all links with hashes
+	$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  });
